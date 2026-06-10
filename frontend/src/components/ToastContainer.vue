ec3e320a61b3e167087f620cafd3e277990ed42c
殷贵庆 <cplasfyin@163.com>
2026-06-10 16:09:41 +0800

    fix: ToastContainer 添加 role=alert 无障碍属性 + break-words 防溢出

--- frontend/src/components/ToastContainer.vue
+++ frontend/src/components/ToastContainer.vue
@@ -9,6 +9,7 @@
       :key="t.id"
       :class="['alert', `alert-${t.type}`]"
       class="shadow-lg"
+      role="alert"
     >
       <!-- success icon: 勾选圆圈 -->
       <svg
@@ -58,7 +59,7 @@
               d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
 
-      <span>{{ t.message }}</span>
+      <span class="break-words">{{ t.message }}</span>
 
       <!-- error 手动关闭按钮 -->
       <button
